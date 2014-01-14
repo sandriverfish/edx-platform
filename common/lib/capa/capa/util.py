ba23807ad7e3178aa6cf9586088916fbb49b481e
Alexander Kryklia <kryklia@gmail.com>
2014-01-14 12:27:06 +0200

    Set clearer tolerance.

--- common/lib/capa/capa/util.py
+++ common/lib/capa/capa/util.py
@@ -8,7 +8,7 @@ from cmath import isinf
 #
 # Utility functions used in CAPA responsetypes
 
-default_tolerance = '1e-3%'
+default_tolerance = '0.001%'
 
 def compare_with_tolerance(v1, v2, tol=default_tolerance):
     '''
